a487cd56479c36ae84c3aea1b433d4b2ec068404
Randall Granier <rgranier@gmail.com>
2020-12-13 17:13:38 -0500

    Note about clean env first using latest.

--- scripts/dockerProdDeploy.sh
+++ scripts/dockerProdDeploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Deploy in detached mode
+# We can use the latest, but we need to clean the environment first.
 #docker container run -d --name webappcontainer -p 8080:80 rgranier/webapprepo:v1.3
 docker container run -d --name webappcontainer -p 8080:80 rgranier/webapprepo:latest
 
