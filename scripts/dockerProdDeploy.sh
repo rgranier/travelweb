8ece9f276508c1965b4f26a3152832307a597140
Randall Granier <rgranier@gmail.com>
2020-12-14 07:36:30 -0500

    Prod deployment script should be port 80, not 8080, which will be local deployment.

--- scripts/dockerProdDeploy.sh
+++ scripts/dockerProdDeploy.sh
@@ -3,7 +3,7 @@
 # Deploy in detached mode
 # We can use the latest, but we need to clean the environment first.
 #docker container run -d --name webappcontainer -p 8080:80 rgranier/webapprepo:v1.3
-docker container run -d --name webappcontainer -p 8080:80 rgranier/webapprepo:latest
+docker container run -d --name webappcontainer -p 80:80 rgranier/webapprepo:latest
 
 # List the containers
 echo ""
